928f765da666772d883f813ab887865040d92193
Godwin-88 <godwinedgar@gmail.com>
2024-11-12 20:03:02 +0300

    to do app

---
+++ todo_app/target/debug/deps/todo_app-dc56978be40f6ca1.d
@@ -0,0 +1,5 @@
+/home/ed/projectsrust/todo_app/target/debug/deps/todo_app-dc56978be40f6ca1: src/main.rs
+
+/home/ed/projectsrust/todo_app/target/debug/deps/todo_app-dc56978be40f6ca1.d: src/main.rs
+
+src/main.rs:
---
+++ todo_app/target/debug/todo_app.d
@@ -0,0 +1,1 @@
+/home/ed/projectsrust/todo_app/target/debug/todo_app: /home/ed/projectsrust/todo_app/src/main.rs
