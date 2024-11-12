1e7461e72fe0d0871fadfae437f7cc9f0d068805
Godwin-88 <godwinedgar@gmail.com>
2024-11-12 20:40:23 +0300

    calculator app

---
+++ calculator_app/target/debug/calculator_app.d
@@ -0,0 +1,1 @@
+/home/ed/projectsrust/calculator_app/target/debug/calculator_app: /home/ed/projectsrust/calculator_app/src/main.rs
---
+++ calculator_app/target/debug/deps/calculator_app-208fb8c7fb7c4aaf.d
@@ -0,0 +1,5 @@
+/home/ed/projectsrust/calculator_app/target/debug/deps/calculator_app-208fb8c7fb7c4aaf: src/main.rs
+
+/home/ed/projectsrust/calculator_app/target/debug/deps/calculator_app-208fb8c7fb7c4aaf.d: src/main.rs
+
+src/main.rs:
